5f46e0496d1b066c09f1606a6f9eef437da25a66
Your Name <108307350+pjduri@users.noreply.github.com>
2023-08-28 11:58:12 -0500

    removed unnecessary conditional

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -53,12 +53,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -84,7 +84,7 @@ public class JobData {
      * Search all columns for the given term
      *
      * @param value The search term to look for
-     * @return      List of all jobs with at least one field containing the value
+     * @return List of all jobs with at least one field containing the value
      */
     public static ArrayList<HashMap<String, String>> findByValue(String value) {
 
@@ -93,13 +93,11 @@ public class JobData {
         ArrayList<HashMap<String, String>> someJobs = new ArrayList<>();
 
         for (HashMap<String, String> job : allJobs) {
-            if (!someJobs.contains(job)) {
-                for (String key : job.keySet()) {
-                    String aValue = job.get(key);
-                    if (aValue.toLowerCase().contains(value.toLowerCase())) {
-                        someJobs.add(job);
-                        break;
-                    }
+            for (String key : job.keySet()) {
+                String aValue = job.get(key);
+                if (aValue.toLowerCase().contains(value.toLowerCase())) {
+                    someJobs.add(job);
+                    break;
                 }
             }
         }
